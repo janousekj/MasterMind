13bf03c6c9c38fffa27bf2ea7b3000be306a4acd
janousekj <janousek_j@volny.cz>
2015-12-03 09:06:27 +0100

    Change color
    Change one color in treasure to orange

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
         List<Color> colorList = new ArrayList<>();
 
-        colorList.add(Color.BLUE);
+        colorList.add(Color.ORANGE);
         colorList.add(Color.RED);
         colorList.add(Color.GREEN);
         colorList.add(Color.ORANGE);
